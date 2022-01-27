edbd13879d31cb599a0d7a8b4efbf6e4cf9424bf
DEVGEEKTM <ismailostmar@gmail.com>
2022-01-27 03:03:19 +0100

    Add Security Config withBcrypt and add Controllers

--- src/main/java/org/sid/SecurityService/Sec/Entities/AppUser.java
+++ src/main/java/org/sid/SecurityService/Sec/Entities/AppUser.java
@@ -1,5 +1,6 @@
 package org.sid.SecurityService.Sec.Entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ public class AppUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     @ManyToMany(fetch = FetchType.EAGER) // des que je charge un User j'ai les roles de cette utilisateur de cette collection
     private Collection<AppRole> appRoles= new ArrayList<>();
--- src/main/java/org/sid/SecurityService/Sec/web/AccountRestController.java
+++ src/main/java/org/sid/SecurityService/Sec/web/AccountRestController.java
@@ -1,8 +1,12 @@
 package org.sid.SecurityService.Sec.web;
 
+import lombok.Data;
+import org.sid.SecurityService.Sec.Entities.AppRole;
 import org.sid.SecurityService.Sec.Entities.AppUser;
 import org.sid.SecurityService.Sec.Services.AccountService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,4 +24,26 @@ public class AccountRestController {
     public List<AppUser> appUsers(){
         return accountService.listUsers();
     }
+
+    @PostMapping(path = "/users")
+        public AppUser saveUser(@RequestBody AppUser appUser){
+            return accountService.addNewUser(appUser);
+        }
+
+    @PostMapping(path = "/roles")
+        public AppRole saveRole(@RequestBody AppRole appRole){
+        return  accountService.addNewRole(appRole);
+        }
+
+    @PostMapping(path = "/addRoleToUser")
+    public void addRoleToUser(@RequestBody RoleUserForm roleUserForm){
+        accountService.addRoleToUser(roleUserForm.getUsername(), roleUserForm.getRolename());
+    }
+
+    @Data
+    class RoleUserForm{
+        private String username;
+        private String rolename;
+    }
+
 }
