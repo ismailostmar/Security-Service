801921adc62d1bf49d1ef76dfb08cbd73d656e5c
DEVGEEKTM <ismailostmar@gmail.com>
2022-01-29 02:59:38 +0100

    to Make Low Code

---
+++ src/main/java/org/sid/SecurityService/Sec/JWTUtile.java
@@ -0,0 +1,10 @@
+package org.sid.SecurityService.Sec;
+
+public class JWTUtile {
+    public static final String SECRET="mySecret1234";
+    public static final String AUTH_HEADER="Authorization";
+    public static final long EXPIRE_ACCESS_TOKEN=2*60*1000;
+    public static final long EXPIRE_REFRESH_TOKEN=15*60*1000;
+    public static final String PREFIX="Bearer ";
+
+}
