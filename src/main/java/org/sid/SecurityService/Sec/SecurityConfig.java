ec7ff9c6708521057b4da9785c2e092c9e6d7f0e
DEVGEEKTM <ismailostmar@gmail.com>
2022-01-29 03:01:20 +0100

    Clean Code

--- src/main/java/org/sid/SecurityService/Sec/SecurityConfig.java
+++ src/main/java/org/sid/SecurityService/Sec/SecurityConfig.java
@@ -1,56 +1,34 @@
 package org.sid.SecurityService.Sec;
 
-import org.sid.SecurityService.Sec.Entities.AppUser;
 import org.sid.SecurityService.Sec.Filters.JwtAuthenticationFilter;
 import org.sid.SecurityService.Sec.Filters.JwtAuthorizationFilter;
-import org.sid.SecurityService.Sec.Services.AccountService;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+
+import org.sid.SecurityService.Sec.Services.UserDetailsServiceImpl;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private AccountService accountService;
+    private UserDetailsServiceImpl userDetailsService;
 
-    public SecurityConfig(AccountService accountService) {
-        this.accountService = accountService;
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        // Statful Authentication with Session ID
-        // User details Service for Security Access using the User Spring
-        auth.userDetailsService(new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                AppUser appUser = accountService.loadUserByUsername(username);
-                Collection<GrantedAuthority> authorities = new ArrayList<>();
-                appUser.getAppRoles().forEach(r-> {
-                    authorities.add(new SimpleGrantedAuthority(r.getRoleName()));
-                });
-                return new User(appUser.getUsername(), appUser.getPassword(),authorities);
-            }
-        });
+        auth.userDetailsService(userDetailsService);
     }
 
     // Authentication Statless
