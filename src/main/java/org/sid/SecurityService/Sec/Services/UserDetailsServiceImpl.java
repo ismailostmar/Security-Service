d18c1c351e75f035e2af4f82864c5275f8397da1
DEVGEEKTM <ismailostmar@gmail.com>
2022-01-29 03:01:36 +0100

    Clean Code

---
+++ src/main/java/org/sid/SecurityService/Sec/Services/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package org.sid.SecurityService.Sec.Services;
+
+import org.sid.SecurityService.Sec.Entities.AppUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private AccountService accountService;
+
+
+    public UserDetailsServiceImpl(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    // Stateful Authentication with Session ID
+    // User details Service for Security Access using the User Spring
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = accountService.loadUserByUsername(username);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        appUser.getAppRoles().forEach(r-> {
+            authorities.add(new SimpleGrantedAuthority(r.getRoleName()));
+        });
+        return new User(appUser.getUsername(), appUser.getPassword(),authorities);
+
+    }
+}
